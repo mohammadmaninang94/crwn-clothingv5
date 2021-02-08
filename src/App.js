480ff6654997cd56fdf9a34953e28d9bbbcecaa3
Mohammad Maninang <mohammadmaninang94@gmail.com>
2021-02-08 17:12:00 +0800

    added header and links to home, shop and contact page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/home-page/home-page.component';
 import ShopPage from './pages/shop-page/shop-page.component';
 
@@ -8,8 +9,9 @@ import './App.scss';
 function App() {
   return (
     <div className="container">
+      <Header />
       <Switch>
-        <Route exact path='/' render={() => <HomePage />}/>
+        <Route exact path='/' render={() => <HomePage />} />
         <Route path='/shop' render={() => <ShopPage />} />
       </Switch>
     </div>
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,21 @@
-const Header = () => (
+import { NavLink } from 'react-router-dom';
 
+import { ReactComponent as Logo } from '../../assests/image/4.2 crown.svg';
+
+import './header.styles.scss';
+
+const Header = () => (
+    <header className='header'>
+        <NavLink className='header__link header__link-logo' activeClassName='header__link--active' exact to='/'>
+            <Logo />
+        </NavLink>
+        <nav className='header__nav-links'>
+            <NavLink className='header__link' activeClassName='header__link--active' to='/shop'>shop</NavLink>
+            <NavLink className='header__link' activeClassName='header__link--active' to='/contact'>contact</NavLink>
+            <NavLink className='header__link' activeClassName='header__link--active' to='/signin'>sign in</NavLink>
+            <NavLink className='header__link' activeClassName='header__link--active' to='/'>cart</NavLink>
+        </nav>
+    </header>
 );
 
 export default Header;

49e48c51906633e7bea4fe46de7b17d307b9b810
Mohammad Maninang <mohammadmaninang94@gmail.com>
2021-02-22 16:47:42 +0800

    setup redux in app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,9 +12,11 @@ import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

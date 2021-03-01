d54a1545dcc34a51d0523880dc1bbec88f6a43e3
Mohammad Maninang <mohammadmaninang94@gmail.com>
2021-03-01 10:20:35 +0800

    added redux persist and persisted state.cart and added directory state to redux

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,31 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../menu-item/menu-item.components';
 
-import SECTIONS_DATA from '../../assests/data/sections.data';
-
 import "./directory.styles.scss";
 
-class Directory extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            sections: SECTIONS_DATA
+const Directory = ({ sections }) => (
+    <div className='directory-menu'>
+        {
+            sections.map(({ title, imageUrl, id }) => (
+                <MenuItem key={id} title={title} imageUrl={imageUrl} />
+            ))
         }
-    }
+    </div>
+);
 
-    render() {
-        return (
-            <div className='directory-menu'>
-                {
-                    this.state.sections.map(({ title, imageUrl, id }) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} />
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+})
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);

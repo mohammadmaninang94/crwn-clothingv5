64c96071d50a04a24af90b23f4ecebf667b9a510
Mohammad Maninang <mohammadmaninang94@gmail.com>
2021-03-18 11:41:44 +0800

    typo in setting up build path

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-    app.search(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function (req, res) {
         res.sendfile(path.join(__dirname, 'client/build', 'index.html'));

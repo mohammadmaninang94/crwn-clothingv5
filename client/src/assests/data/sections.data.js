0b86a4672ffc295dc49ff1b4dc124c313e722e81
Mohammad Maninang <mohammadmaninang94@gmail.com>
2021-11-04 09:11:13 +0800

    added imageUrlWebp src

--- client/src/assests/data/sections.data.js
+++ client/src/assests/data/sections.data.js
@@ -5,6 +5,7 @@ const SECTIONS_DATA = [
         title: 'hats',
         // imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
         imageUrl: 'images/hats.png', // local file in public/images/
+        imageUrlWebp: 'images/hats.webp', // local file in public/images/
         id: 1,
         linkUrl: 'shop/hats'
 
@@ -13,6 +14,7 @@ const SECTIONS_DATA = [
         title: 'jackets',
         // imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
         imageUrl: 'images/jackets.png', // local file in public/images/
+        imageUrlWebp: 'images/jackets.webp', // local file in public/images/
         id: 2,
         linkUrl: 'shop/jackets'
     },
@@ -20,6 +22,7 @@ const SECTIONS_DATA = [
         title: 'sneakers',
         // imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
         imageUrl: 'images/sneakers.png', // local file in public/images/
+        imageUrlWebp: 'images/sneakers.webp', // local file in public/images/
         id: 3,
         linkUrl: 'shop/sneakers'
     },
@@ -27,6 +30,7 @@ const SECTIONS_DATA = [
         title: 'womens',
         // imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
         imageUrl: 'images/womens.png', // local file in public/images/
+        imageUrlWebp: 'images/womens.webp', // local file in public/images/
         size: 'large',
         id: 4,
         linkUrl: 'shop/womens'
@@ -35,6 +39,7 @@ const SECTIONS_DATA = [
         title: 'mens',
         // imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
         imageUrl: 'images/men.png', // local file in public/images/
+        imageUrlWebp: 'images/men.webp', // local file in public/images/
         size: 'large',
         id: 5,
         linkUrl: 'shop/mens'

00c0a76181647c46e7eab69a1715bd721edc58db
Mohammad Maninang <mohammadmaninang94@gmail.com>
2022-02-10 09:26:06 +0800

    Fixed Unit testing and snapshots

--- client/src/components/directory/directory.test.js
+++ client/src/components/directory/directory.test.js
@@ -1,9 +1,41 @@
+import { render } from '@testing-library/react';
+import { MemoryRouter } from "react-router-dom";
+import renderer from 'react-test-renderer';
+
 import Directory from './directory.component';
-import sections from '../../assests/data/sections.data'; 
+import sections from '../../assests/data/sections.data';
 
-import { shallow } from 'enzyme';
+describe('Directory component', () => {
+    describe('Renders with', () => {
+        let wrapper = null;
 
-test('Render Directory', () => {
-    const directory = shallow(<Directory sections={sections} />);
-    expect(directory).toMatchSnapshot();
+        beforeEach(() => {
+            wrapper = render(
+                <MemoryRouter>
+                    <Directory sections={sections} />
+                </MemoryRouter>
+            );
+            console.log('beforeAll', wrapper);
+        });
+
+        test(`exact titles from props`, () => {
+            for (var i = 0; sections.length > i; i++) {
+                expect(wrapper.queryByTitle(sections[0].title)).toBeInTheDocument();
+            }
+        });
+
+        test(`${sections.length} menu item component`, () => {
+            console.log('test 2', wrapper);
+            expect(wrapper.queryAllByRole('heading')).toHaveLength(sections.length);
+        });
+    });
+
+    test('renders correctly', () => {
+        const tree = renderer
+            .create(<MemoryRouter>
+                <Directory sections={sections} />
+            </MemoryRouter>)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 });
